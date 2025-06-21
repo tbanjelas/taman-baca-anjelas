c43505ce958f84f06062be382e4fba9e9ee928c5
tbanjelas <tbmanjelas@gmail.com>
2025-06-21 20:44:38 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,8 @@
 module.exports = function(eleventyConfig) {
   return {
     dir: {
-      input: ".",         // direktori input (semua file konten kamu di sini)
-      includes: "src/layouts", // folder untuk layout/template
-      output: "_site"     // hasil build
+      input: "src",
+      output: "_site"
     }
   };
 };
